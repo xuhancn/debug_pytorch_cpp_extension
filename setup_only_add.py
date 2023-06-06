b057304ef0dacdade1af4f5ccedf4aa00b865561
Han, Xu <xu.han@intel.com>
2023-06-06 20:54:48 +0000

    update code.

--- setup_only_add.py
+++ setup_only_add.py
@@ -2,21 +2,27 @@ import os
 import torch
 from setuptools import setup
 
+import time
+
 from torch.utils.cpp_extension import BuildExtension, CppExtension
 
 torch_path = os.path.dirname(torch.__file__)
 print(torch_path)
 
-PACKAGE_NAME = "dummy_ext"
+PACKAGE_NAME = "only_add"
 
+start = time.time()
+print('!!!!!compiling start.')
 setup(
     name='PACKAGE_NAME',
     ext_modules=[
-        CppExtension('mod_test_add_ninja', [
-            'src/main.cpp',
+        CppExtension('PACKAGE_NAME', [
+            'src/only_add.cpp',
         ], extra_compile_args=[])
     ],
     cmdclass={
         'build_ext': BuildExtension.with_options(use_ninja=True)
     }
 )
+end = time.time()
+print('!!!!!compiling time: ',end - start)
