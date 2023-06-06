d198533f83e03915f61cdb58598296fd47020a8e
Han, Xu <xu.han@intel.com>
2023-06-06 21:00:26 +0000

    add pybind11 case.

---
+++ setup_add_pybind.py
@@ -0,0 +1,28 @@
+import os
+import torch
+from setuptools import setup
+
+import time
+
+from torch.utils.cpp_extension import BuildExtension, CppExtension
+
+torch_path = os.path.dirname(torch.__file__)
+print(torch_path)
+
+PACKAGE_NAME = "add_pybind"
+
+start = time.time()
+print('!!!!!compiling start.')
+setup(
+    name='PACKAGE_NAME',
+    ext_modules=[
+        CppExtension('PACKAGE_NAME', [
+            'src/add_pybind.cpp',
+        ], extra_compile_args=[])
+    ],
+    cmdclass={
+        'build_ext': BuildExtension.with_options(use_ninja=True)
+    }
+)
+end = time.time()
+print('!!!!!compiling time: ',end - start)
